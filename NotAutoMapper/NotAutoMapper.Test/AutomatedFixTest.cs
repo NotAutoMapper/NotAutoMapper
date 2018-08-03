042bdc8793abefad9d3f8126ccf84194cab893f5
deaddog <mikkelslsl@gmail.com>
2018-08-03 21:56:30 +0200

    Added new test for automation of source files.

---
+++ NotAutoMapper/NotAutoMapper.Test/AutomatedFixTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace NotAutoMapper.Test
+{
+    [TestClass]
+    public class AutomatedFixTest : CodeFixVerifier
+    {
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new NotAutoMapperCodeFixProvider();
+        }
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new NotAutoMapperAnalyzer();
+        }
+    }
+}
