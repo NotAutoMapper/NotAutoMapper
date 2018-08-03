c428b10beb78298be7b916a8dc488260bf18b048
deaddog <mikkelslsl@gmail.com>
2018-08-03 22:02:47 +0200

    Test meta (location and message) parsed from comments.

--- NotAutoMapper/NotAutoMapper.Test/AutomatedFixTest.cs
+++ NotAutoMapper/NotAutoMapper.Test/AutomatedFixTest.cs
@@ -12,6 +12,40 @@ namespace NotAutoMapper.Test
     [TestClass]
     public class AutomatedFixTest : CodeFixVerifier
     {
+        private struct Location
+        {
+            public static Location FromComments(IImmutableList<string> comments)
+            {
+                return comments
+                    .Select(x => Regex.Match(x, "^(?://)? *FixLocation *: *(?:ln|line):? *(?<line>\\d+),? *(?:col|column):? *(?<column>\\d+) *$", RegexOptions.IgnoreCase))
+                    .Where(m => m.Success)
+                    .Select(x => new Location
+                    (
+                        line: int.Parse(x.Groups["line"].Value),
+                        column: int.Parse(x.Groups["column"].Value)
+                    ))
+                    .Single();
+            }
+
+            public Location(int line, int column)
+            {
+                Line = line;
+                Column = column;
+            }
+
+            public int Line { get; }
+            public int Column { get; }
+        }
+
+        private static string MessageFromComments(IImmutableList<string> comments)
+        {
+            return comments
+                .Select(x => Regex.Match(x, "^(?://)? *MESSAGE *: *(?<message>[^ ]|[^ ].*[^ ]) *$", RegexOptions.IgnoreCase))
+                .Where(m => m.Success)
+                .Select(x => x.Groups["message"].Value)
+                .Single();
+        }
+
         [TestMethod]
         public void TestTestCases()
         {
@@ -21,6 +55,9 @@ namespace NotAutoMapper.Test
             {
                 var content = File.ReadAllLines(f);
                 var topComments = content.TakeWhile(x => Regex.IsMatch(x, "^//.*$")).ToImmutableList();
+
+                var location = Location.FromComments(topComments);
+                var message = MessageFromComments(topComments);
             }
         }
 
