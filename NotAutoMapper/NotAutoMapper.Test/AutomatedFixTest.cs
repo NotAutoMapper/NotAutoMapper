d3ef7fe3bc9ff3c3e22b08511db5898a4a0f27f1
deaddog <mikkelslsl@gmail.com>
2018-08-03 22:00:43 +0200

    Extracted comments at the top of the file.

--- NotAutoMapper/NotAutoMapper.Test/AutomatedFixTest.cs
+++ NotAutoMapper/NotAutoMapper.Test/AutomatedFixTest.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using TestHelper;
 
 namespace NotAutoMapper.Test
@@ -8,6 +12,18 @@ namespace NotAutoMapper.Test
     [TestClass]
     public class AutomatedFixTest : CodeFixVerifier
     {
+        [TestMethod]
+        public void TestTestCases()
+        {
+            var files = Directory.EnumerateFiles("TestCases", "*.cs");
+
+            foreach (var f in files)
+            {
+                var content = File.ReadAllLines(f);
+                var topComments = content.TakeWhile(x => Regex.IsMatch(x, "^//.*$")).ToImmutableList();
+            }
+        }
+
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
             return new NotAutoMapperCodeFixProvider();
